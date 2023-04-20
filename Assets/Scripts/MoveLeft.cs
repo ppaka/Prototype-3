b7053acda5122649aba384b7c53995dc9aa952a3
ppaka <asj0216@gmail.com>
2023-04-20 12:17:03 +0900

    2023-04-20

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
